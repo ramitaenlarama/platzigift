5725520412cb19fee0438fbf96a4509dd59b1311
Cristian Ramanzin <ramanzincristian@gmail.com>
2019-12-12 17:34:52 -0500

    Clase 12: Manejo de librerías

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ function init_template(){
     add_theme_support( 'title-tag');
 
 }
+
+
+function assets(){
+    wp_register_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1','all');
+    wp_register_style('montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap','','1.0', 'all');
+    wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
+
+}
+
+add_action('wp_enqueue_scripts','assets');
