039acb6f7810fdd6211c27f1d2d3538091c68243
Cristian Ramanzin <ramanzincristian@gmail.com>
2019-12-12 17:45:28 -0500

    Clase 23: Personalizar el Loop en la página principal

--- front-page.php
+++ front-page.php
@@ -10,6 +10,41 @@
         <?php    }
     }?>
 
+    <div class="lista-productos my-5">
+        <h2 class='text-center'>PRODUCTOS</h2>
+        <div class="row">
+        <?php
+        $args = array(
+            'post_type' => 'producto',
+            'post_per_page' => -1, 
+            'order'         => 'ASC',
+            'orderby'       => 'title'
+        );
+
+        $productos = new WP_Query($args);
+
+        if($productos->have_posts()){
+            while($productos->have_posts()){
+                $productos->the_post();
+                ?>
+
+                <div class="col-4">
+                    <figure>
+                        <?php the_post_thumbnail('large'); ?>
+                    </figure>
+                    <h4 class='my-3 text-center'>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title();?>
+                        </a>
+                    </h4>
+                </div>
+
+           <?php }
+        }
+
+        ?>
+      </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
