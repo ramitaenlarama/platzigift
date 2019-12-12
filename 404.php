8569675963ebd3c1dc4fd68cf2631c7f93f3883c
Cristian Ramanzin <ramanzincristian@gmail.com>
2019-12-12 17:45:57 -0500

    Clase 26: Página 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+<main class='container'>
+    <div class="pagina404 my-5">
+        <h1>404 PÁGINA NO ENCOTRADA</h1>
+        <h2>Haga <a href="<?php echo home_url(); ?>">click aquí</a>  para volver a la página principal</h2>
+    </div>
+</main>
+
+<?php get_footer() ?>
