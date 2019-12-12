1432b2f6466717698a53046168813315e6de8460
Cristian Ramanzin <ramanzincristian@gmail.com>
2019-12-12 17:36:03 -0500

    Clase 13: Agregando Javascript al tema

--- functions.php
+++ functions.php
@@ -12,7 +12,10 @@ function assets(){
     wp_register_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1','all');
     wp_register_style('montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap','','1.0', 'all');
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
-
+   
+    wp_register_script('popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js','','1.16.0', true);
+    wp_enqueue_script('boostraps', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery','popper'),'4.4.1', true);
+    wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/custom.js', '', '1.0', true);
 }
 
 add_action('wp_enqueue_scripts','assets');
--- header.php
+++ header.php
@@ -8,5 +8,13 @@
 </head>
 <body>
 
-
+<header>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="logo">
+            </div>
+        </div>
+    </div>
+</header>
     
